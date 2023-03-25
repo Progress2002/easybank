a07ffcd1a2370dee3602a3e6d1f1bc94418c3129
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-03-25 09:04:39 +0100

    Create Data.js file

---
+++ src/Components/Data.js
@@ -0,0 +1,64 @@
+// import onlineIMG from "../images/icon-online.svg"
+
+export const aboutData = [
+  {
+    id: "1",
+    imgURL: "/static/media/icon-online.b0622b15741418e9efe3805d1b2ac26b.svg",
+    title: "Online Banking",
+    summary: "Our modern web mobile application allows you to keep track of your finances wherever you are in the world."
+  },
+  {
+    id: "2",
+    imgURL: "/static/media/icon-budgeting.ad7169975ea1eda87072ea0325a8204a.svg",
+    title: "Simple Budgeting",
+    summary: "See exactly where your money goes each month. Receive notifications when you're close to your hitting limits."
+  },
+  {
+    id: "3",
+    imgURL: "/static/media/icon-onboarding.d74aed6429619336a30e42334d6eb432.svg",
+    title: "Fast Onboarding",
+    summary: "We don't do branches,  Open your account in minutes and start taking control of your finances right away."
+  },
+  {
+    id: "4",
+    imgURL: "static/media/icon-api.74c8ad1751bbd1e7f545e458f9306885.svg",
+    title: "Open API",
+    summary: "Manage your savings, investments, pensions and much more from one account. Tracking your money has never been easier."
+  },
+]
+
+export const articleData = [
+  {
+    id: "1",
+    alt: "image-currency",
+    imgUrl: "/static/media/image-currency.1b385bf9f62bf25ef22d.jpg",
+    author: "By Claire Robinson",
+    title: "Receive money in any currency with no fees",
+    summary: "The world is getting smaller and we're becoming more mobile. So why should you be forced to only receive money in a single..."
+  },
+  {
+    id: "2",
+    alt: "image-restaurant",
+    imgUrl: "/static/media/image-restaurant.1951b57675babfd83c6c.jpg",
+    author: "By Wilson Hutton",
+    title: "Treat yourself without worrying about money",
+    summary: "Our simple budgeting features allows you to separate out your spending and set realistic limits each month. That means you... "
+  },
+  {
+    id: "3",
+    alt: "image-plane",
+    imgUrl: "/static/media/image-plane.03db115dff75876c3f3d.jpg",
+    author: "By Wilson Hutton",
+    title: "Take your Easybank card wherever you go",
+    summary: "We want you to enjoy your travels. This is why we don't charge any fees on purchases while you're abroad. We'll even show you..."
+  },
+  {
+    id: "4",
+    alt: "image-confetti",
+    imgUrl: "/static/media/image-confetti.67921b1124297023a080.jpg",
+    author: "By Claire Robinson",
+    title: "Our invite-only Beta accounts are now live!",
+    summary: "After a lot of heard work by the whole tea,, er're excited to launch our closed beta. It's easy to request an invitee through the site..."
+  }
+]
+
