4fff649785a47065d5ac04dc51458a9199220b6d
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-03-25 09:05:24 +0100

    Remove comments from tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
