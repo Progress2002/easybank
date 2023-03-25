f88dec4fd80991de41edec5aafd00ff4f47544f7
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-03-25 09:10:04 +0100

    Create About component and import Blog component

---
+++ src/Components/About.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Item from './Item'
+import { aboutData } from "./Data"
+
+
+const About = () => {
+  return (
+   <section className='bg-LightGrayishBlue'>
+       <div className='mt-20 md:mt-0 md:pt-24 py-20 md:mt: md:container'  id='about'>
+      <div className='text-center px-5'>
+        <h2 className='text-center md:text-left text-4xl text-DarkBlue leading-tight mb-5'>
+          Why choose Easybank?
+        </h2>
+        <p className='text-GrayishBlue text-center mb-16 md:text-left md:w-1/2'>
+          We leverage Open Banking to turn your bank account into your financial hub. Control your finance like never before.
+        </p>
+        <ul className='md:flex md:text-left gap-16'>
+          {
+            aboutData.map(data => {
+              const {id, imgURL, summary, title} = data;
+              return <Item key={id} imgURL={imgURL} summary={summary} title={title} />
+            })
+          }
+        </ul>
+      </div>
+    </div>
+   </section>
+  )
+}
+
+export default About
