aaca630a1902374ca3d84d5d88f0bd868e7bad6e
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-03-26 03:36:11 +0100

    Create footer links component

---
+++ src/Components/FooterLink.js
@@ -0,0 +1,18 @@
+import { Reveal } from "react-reveal";
+
+const FooterLink = (props) => {
+  const { value, href } = props;
+  return (
+    <Reveal>
+      <li>
+        <a
+          className="block font-bold text-slate-300 hover:text-LimeGreen"
+          href={href}>
+          {value}
+        </a>
+      </li>
+    </Reveal>
+  );
+};
+
+export default FooterLink;
