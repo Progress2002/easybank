7147a66228b3ae71ddfda8968285c0feb2802147
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-03-26 03:35:43 +0100

    Create  footer SocialIcon component

---
+++ src/Components/SocialIcons.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { AiFillFacebook } from "react-icons/ai";
+import { AiFillYoutube } from "react-icons/ai";
+import { BsTwitter } from "react-icons/bs";
+import { GrInstagram } from "react-icons/gr";
+import { BsPinterest } from "react-icons/bs";
+
+const SocialIcons = () => {
+  return (
+    <ul className='flex gap-3 justify-center'>
+      <li>
+        <a className='text-slate-100 text-2xl cursor-pointer hover:text-LimeGreen' href='#1'><AiFillFacebook /></a>
+      </li>
+      <li>
+        <a className='text-slate-100 text-2xl cursor-pointer hover:text-LimeGreen' href='#2'><AiFillYoutube /></a>
+      </li>
+      <li>
+        <a className='text-slate-100 text-2xl cursor-pointer hover:text-LimeGreen' href='#3'><BsTwitter /></a>
+      </li>
+      <li>
+        <a className='text-slate-100 text-2xl cursor-pointer hover:text-LimeGreen' href='#4'><BsPinterest /></a>
+      </li>
+      <li>
+        <a className='text-slate-100 text-2xl cursor-pointer hover:text-LimeGreen' href='#5'><GrInstagram /></a>
+      </li>
+    </ul>
+  )
+}
+
+export default SocialIcons
