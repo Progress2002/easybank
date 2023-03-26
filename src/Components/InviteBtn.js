5466f5db7d0200c6ce82df38e2304506319b9f2e
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-03-26 03:34:20 +0100

    Add mouseHover and mouseOut styling to invite Btn

--- src/Components/InviteBtn.js
+++ src/Components/InviteBtn.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InviteBtn = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleMouseEffect = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <div>
       <button
+        onMouseOver={handleMouseEffect}
+        onMouseOut={handleMouseEffect}
         style={{
-          background:
-            "linear-gradient( to right, hsl(136, 65%, 51%), hsl(192, 70%, 51%))",
+          background: isActive
+            ? "linear-gradient( to right, hsl(136, 65%, 70%), hsl(192, 70%, 70%))"
+            : "linear-gradient( to right, hsl(136, 65%, 51%), hsl(192, 70%, 51%))",
         }}
-        className=" py-3 px-8 text-gray-100  rounded-full text-lg font-bold">
+        className=" py-3 px-8 text-gray-100  rounded-full text-lg font-bold btn">
         Request Invite
       </button>
     </div>
