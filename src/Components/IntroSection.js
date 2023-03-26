abadcd1c42c9584db9d40f940eadd8d143a74b4b
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-03-26 03:33:05 +0100

    Update IntroSection.js file

--- src/Components/IntroSection.js
+++ src/Components/IntroSection.js
@@ -7,7 +7,7 @@ import Zoom from"react-reveal/Zoom"
 const IntroSection = () => {
   return (
     <section
-      className=" intro-section  md:flex md:flex-row-reverse md:justify-between "
+      className=" intro-section  md:flex md:flex-row-reverse md:justify-between font-sans"
       id="home">
       <div className="intro-img md:mt-20 md:-mr-36">
        <Zoom>
@@ -20,7 +20,7 @@ const IntroSection = () => {
       </div>
       <Reveal left>
         <div className=" text-center px-9 md:px-0 md:ml- md:text-left md:ml-36 intro-body">
-          <h1 className="text-4xl font-medium text-DarkBlue leading-tight mb-4  md:text-5xl">
+          <h1 className="text-4xl font-medium text-DarkBlue leading-tight mb-4  md:text-5xl font-serif ">
             Next generation digital banking
           </h1>
           <p className="text-lg text-justify text-gray-400 mb-5 md:text-left">
