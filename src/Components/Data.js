41806e62c00c6b1e9f3ac4a6001d0ef177207689
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-03-26 03:32:29 +0100

    Update image url in Data,js file

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,27 +1,26 @@
-// import onlineIMG from "../images/icon-online.svg"
 
 export const aboutData = [
   {
     id: "1",
-    imgURL: "/static/media/icon-online.b0622b15741418e9efe3805d1b2ac26b.svg",
+    imgURL: "/static/media/icon-online.svg",
     title: "Online Banking",
     summary: "Our modern web mobile application allows you to keep track of your finances wherever you are in the world."
   },
   {
     id: "2",
-    imgURL: "/static/media/icon-budgeting.ad7169975ea1eda87072ea0325a8204a.svg",
+    imgURL: "/static/media/icon-budgeting.svg",
     title: "Simple Budgeting",
     summary: "See exactly where your money goes each month. Receive notifications when you're close to your hitting limits."
   },
   {
     id: "3",
-    imgURL: "/static/media/icon-onboarding.d74aed6429619336a30e42334d6eb432.svg",
+    imgURL: "/static/media/icon-onboarding.svg",
     title: "Fast Onboarding",
     summary: "We don't do branches,  Open your account in minutes and start taking control of your finances right away."
   },
   {
     id: "4",
-    imgURL: "static/media/icon-api.74c8ad1751bbd1e7f545e458f9306885.svg",
+    imgURL: "static/media/icon-api.svg",
     title: "Open API",
     summary: "Manage your savings, investments, pensions and much more from one account. Tracking your money has never been easier."
   },
@@ -31,7 +30,7 @@ export const articleData = [
   {
     id: "5",
     alt: "image-currency",
-    imgUrl: "/static/media/image-currency.1b385bf9f62bf25ef22d.jpg",
+    imgUrl: "/static/media/image-currency.jpg",
     author: "By Claire Robinson",
     title: "Receive money in any currency with no fees",
     summary: "The world is getting smaller and we're becoming more mobile. So why should you be forced to only receive money in a single..."
@@ -39,7 +38,7 @@ export const articleData = [
   {
     id: "6",
     alt: "image-restaurant",
-    imgUrl: "/static/media/image-restaurant.1951b57675babfd83c6c.jpg",
+    imgUrl: "/static/media/image-restaurant.jpg",
     author: "By Wilson Hutton",
     title: "Treat yourself without worrying about money",
     summary: "Our simple budgeting features allows you to separate out your spending and set realistic limits each month. That means you... "
@@ -47,7 +46,7 @@ export const articleData = [
   {
     id: "7",
     alt: "image-plane",
-    imgUrl: "/static/media/image-plane.03db115dff75876c3f3d.jpg",
+    imgUrl: "/static/media/image-plane.jpg",
     author: "By Wilson Hutton",
     title: "Take your Easybank card wherever you go",
     summary: "We want you to enjoy your travels. This is why we don't charge any fees on purchases while you're abroad. We'll even show you..."
@@ -55,7 +54,7 @@ export const articleData = [
   {
     id: "8",
     alt: "image-confetti",
-    imgUrl: "/static/media/image-confetti.67921b1124297023a080.jpg",
+    imgUrl: "/static/media/image-confetti.jpg",
     author: "By Claire Robinson",
     title: "Our invite-only Beta accounts are now live!",
     summary: "After a lot of heard work by the whole tea,, er're excited to launch our closed beta. It's easy to request an invitee through the site..."
